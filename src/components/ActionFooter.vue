16501f11ca3efefc8e654874a1c70c901980d5b7
Mayara Alencar <mayara.rma@gmail.com>
2020-10-17 19:29:11 -0300

    feat: html e css para actionFooter

--- src/components/ActionFooter.vue
+++ src/components/ActionFooter.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>
-    Action Footer
-  </div>
+  <footer>
+    <ml-button label="Cancelar" customed-class="button-tertiary"/>
+    <ml-button label="Salvar rascunho" customed-class="button-outline"/>
+    <ml-button label="Agendar"/>
+  </footer>
 </template>
 
 <script>
-export default {};
+import MlButton from "./MlButton";
+export default {
+  name: "ActionFooter",
+  components: {
+    MlButton
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+footer {
+  background-color: var(--white);
+  display: flex;
+  justify-content: flex-end;
+  padding: var(--space-sm) var(--space-md);
+
+  button:not(:last-child) {
+    margin-right: var(--space-sm);
+  }
+}
+
+</style>
