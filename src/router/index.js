c7189c9234d2c921ed87baa5238b198c3ceddd86
Mayara Alencar <mayara.rma@gmail.com>
2020-10-17 17:43:22 -0300

    feat: definição das rotas para criação de agendamento e listagem

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,16 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/createschedule",
+    name: "CreateSchedule",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ "../views/CreateSchedule.vue")
+  },
+  {
+    path: "/schedules",
+    name: "Schedules",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Schedules.vue")
   }
 ];
 
