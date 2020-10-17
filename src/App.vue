87222be932d999f1ea1589f16fe5ac76082bde20
Mayara Alencar <mayara.rma@gmail.com>
2020-10-17 18:31:31 -0300

    feat: MlHeader como componente fixo na aplicação

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
+    <ml-header />
     <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import MlHeader from "@/components/MlHeader.vue";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  components: {
+    MlHeader
   }
-}
+};
+</script>
+
+<style lang="scss">
+@import "@/styles/global";
 </style>
