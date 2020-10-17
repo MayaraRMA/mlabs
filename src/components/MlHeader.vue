6855cfae4c07d67f2247d5ad805a9894b0574ac3
Mayara Alencar <mayara.rma@gmail.com>
2020-10-17 19:12:48 -0300

    feat: html e css do componente MlHeader

--- src/components/MlHeader.vue
+++ src/components/MlHeader.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>
-    Header
-  </div>
+  <header class="header">
+    <router-link to="/">
+      <img alt="mLabs" src="../assets/logo.png" />
+    </router-link>
+    <div class="user-info">
+      <img alt="foto" src="../assets/tumb.png" />
+      <p>{{ name }}</p>
+    </div>
+  </header>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      name: "Anselmo Carlos"
+    };
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  padding: var(--space-sm) var(--space-md);
+}
+
+.user-info {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  img {
+    margin-right: var(--space-sm);
+  }
+}
+</style>
