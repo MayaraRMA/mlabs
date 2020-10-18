2107d200f3cc0b61d2aa94876387e0acc540dea2
Mayara Alencar <mayara.rma@gmail.com>
2020-10-17 23:48:00 -0300

    feat: import dos icones

--- src/main.js
+++ src/main.js
@@ -2,6 +2,35 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCalendarAlt,
+  faClock,
+  faCloudUploadAlt
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faInstagram,
+  faLinkedin,
+  faYoutube,
+  faPinterest,
+  faTwitter,
+  faFacebookF
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(
+  faCalendarAlt,
+  faClock,
+  faCloudUploadAlt,
+  faInstagram,
+  faLinkedin,
+  faYoutube,
+  faPinterest,
+  faTwitter,
+  faFacebookF
+);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
