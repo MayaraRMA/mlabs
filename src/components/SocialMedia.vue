891ba4dc4890f7bf44e8be4fd0391a3f33800b70
Mayara Alencar <mayara.rma@gmail.com>
2020-10-17 23:56:13 -0300

    feat: html e css do componente socialMedia

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -1,11 +1,61 @@
 <template>
-  <div>
-    Social Media
-  </div>
+  <ml-article title="Redes Sociais">
+    <div class="medias">
+      <template v-for="(icon, index) in icons">
+        <ml-media-icon :key="index" :icon="icon" />
+      </template>
+    </div>
+  </ml-article>
 </template>
 
 <script>
-export default {};
+import MlArticle from "./MlArticle";
+import MlMediaIcon from "./MlMediaIcon";
+
+export default {
+  name: "SocialMedia",
+  components: {
+    MlArticle,
+    MlMediaIcon
+  },
+  data() {
+    return {
+      icons: [
+        {
+          name: "instagram",
+          disable: false
+        },
+        {
+          name: "linkedin",
+          disable: false
+        },
+        {
+          name: "youtube",
+          disable: true
+        },
+        {
+          name: "pinterest",
+          disable: true
+        },
+        {
+          name: "twitter",
+          disable: true
+        },
+        {
+          name: "facebook-f",
+          disable: true
+        }
+      ]
+    };
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.medias {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: var(--space);
+}
+</style>
