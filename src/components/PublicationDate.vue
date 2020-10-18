b7e8816d33fb929a9fdf9a7b82c21add8cdfacce
Mayara Alencar <mayara.rma@gmail.com>
2020-10-18 12:04:16 -0300

    feat: html e css do componete publication-date

--- src/components/PublicationDate.vue
+++ src/components/PublicationDate.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>
-    Publication Date
-  </div>
+  <ml-article title="Data de Publicação">
+    <div class="publication-container">
+      <ml-calendar />
+      <ml-time />
+    </div>
+  </ml-article>
 </template>
 
 <script>
-export default {};
+import MlArticle from "./MlArticle";
+import MlCalendar from "./MlCalendar";
+import MlTime from "./MlTime";
+
+export default {
+  components: {
+    MlArticle,
+    MlCalendar,
+    MlTime
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.publication-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: var(--space-sm);
+
+  div:first-child {
+    margin-right: var(--space);
+  }
+}
+</style>
