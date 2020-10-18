6596cb6a508be7d3fc7383f85160c63383f940a4
Mayara Alencar <mayara.rma@gmail.com>
2020-10-17 23:56:41 -0300

    feat: html e css do postPreview

--- src/components/PostPreview.vue
+++ src/components/PostPreview.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>
-    Post Preview
-  </div>
+  <ml-article title="Vizualização do post ">
+    <div class="no-content">
+      <h2>
+        Aguardando conteúdo. Informe os canais e as mídias desejadas para
+        visualização.
+      </h2>
+      <img src="../assets/post.png" alt="aguardando conteúdo" />
+    </div>
+  </ml-article>
 </template>
 
 <script>
-export default {};
+import MlArticle from "./MlArticle";
+export default {
+  components: {
+    MlArticle
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.no-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: var(--space-xlg);
+
+  h2 {
+    color: var(--gray-dark);
+    font-weight: 500;
+    font-size: 16px;
+    text-align: center;
+    padding: 0 var(--space-xlg);
+    margin-bottom: var(--space-sm);
+  }
+}
+</style>
