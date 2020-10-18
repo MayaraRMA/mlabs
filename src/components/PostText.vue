2975903ff6f382d3c1134ae6ba9ba70028b4b8da
Mayara Alencar <mayara.rma@gmail.com>
2020-10-17 23:57:10 -0300

    feat: html e css do posttext

--- src/components/PostText.vue
+++ src/components/PostText.vue
@@ -1,11 +1,40 @@
 <template>
-  <div>
-    Post Text
-  </div>
+  <ml-article title="Texto do post">
+    <div class="post">
+      <textarea
+        name="post"
+        id="post"
+        cols="30"
+        rows="5"
+        placeholder="Aqui vai o texto descritivo desse post"
+      ></textarea>
+    </div>
+  </ml-article>
 </template>
 
 <script>
-export default {};
+import MlArticle from "./MlArticle";
+export default {
+  components: {
+    MlArticle
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.post {
+  padding: var(--space-sm);
+
+  textarea {
+    width: 100%;
+    resize: none;
+    border: var(--border-icon);
+    border-radius: var(--space-xxs);
+    padding: var(--space-xxs);
+
+    &::-webkit-input-placeholder {
+      color: var(--gray);
+    }
+  }
+}
+</style>
