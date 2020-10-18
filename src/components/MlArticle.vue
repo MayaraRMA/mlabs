a71055161bd7294fdfa7512f406455c6c47cb691
Mayara Alencar <mayara.rma@gmail.com>
2020-10-17 23:51:13 -0300

    feat: novo componente - MlArticle

---
+++ src/components/MlArticle.vue
@@ -0,0 +1,29 @@
+<template>
+  <article>
+    <h1>{{ title }}</h1>
+    <slot></slot>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+article {
+  background-color: var(--white);
+  border-radius: var(--space-xxs);
+
+  h1 {
+    padding: var(--space-xxs) var(--space-sm);
+    border-bottom: var(--border-gray);
+    font-weight: 500;
+  }
+}
+</style>
