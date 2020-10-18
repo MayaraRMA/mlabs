bab23aa97fc7ae1fb08f77775a4f120c5e5de896
Mayara Alencar <mayara.rma@gmail.com>
2020-10-18 12:03:21 -0300

    feat: criação do componente ml-time utilizando date-picker

---
+++ src/components/MlTime.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="time-container">
+    <font-awesome-icon :icon="['fas', 'clock']" />
+    <date-picker
+      v-model="time1"
+      valueType="format"
+      format="HH:MM"
+      type="time"
+      :lang="lang"
+      :append-to-body="false"
+      placeholder="HH:MM"
+      :popup-style="{ 'display': 'none' }"
+    >
+      <template slot="icon-calendar">
+        <p></p>
+      </template>
+    </date-picker>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import DatePicker from "vue2-datepicker";
+import "vue2-datepicker/locale/pt-br";
+
+export default {
+  components: { FontAwesomeIcon, DatePicker },
+  data() {
+    return {
+      time1: null
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.time-container {
+  border: var(--border-icon);
+  border-radius: var(--border-radius);
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  svg {
+    margin-left: var(--space-xs);
+  }
+}
+</style>
+<style src="vue2-datepicker/index.css"></style>
+<style lang="scss">
+.mx-input {
+  outline: none;
+  border: none;
+}
+</style>
