f563d9aefc03ed33a3344775403f69663cc26ed5
Mayara Alencar <mayara.rma@gmail.com>
2020-10-21 07:54:37 -0300

    feat: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest"
+  preset: "@vue/cli-plugin-unit-jest",
+  moduleFileExtensions: ["js", "jsx", "json", "vue"],
+  transform: {
+    "^.+\\.vue$": "vue-jest",
+    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
+    "^.+\\.jsx?$": "babel-jest"
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/**/**.vue"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text-summary", "lcov"]
 };
