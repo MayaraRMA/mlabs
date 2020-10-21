99957279eb04084fab2756d5d2c25fce1bad87f3
Mayara Alencar <mayara.rma@gmail.com>
2020-10-20 23:11:53 -0300

    feat: publication date integrado com a store

--- src/components/PublicationDate.vue
+++ src/components/PublicationDate.vue
@@ -1,8 +1,8 @@
 <template>
   <ml-article title="Data de Publicação">
     <div class="publication-container">
-      <ml-calendar />
-      <ml-time />
+      <ml-calendar v-model="date" />
+      <ml-time v-model="time" />
     </div>
   </ml-article>
 </template>
@@ -11,6 +11,7 @@
 import MlArticle from "./MlArticle";
 import MlCalendar from "./MlCalendar";
 import MlTime from "./MlTime";
+import { mapMutations, mapGetters } from "vuex";
 
 export default {
   name: "PublicationDate",
@@ -18,6 +19,34 @@ export default {
     MlArticle,
     MlCalendar,
     MlTime
+  },
+  computed: {
+    ...mapGetters("post", {
+      get_time: "time",
+      get_date: "date"
+    }),
+    date: {
+      get() {
+        return this.get_date;
+      },
+      set(value) {
+        this.set_date(value);
+      }
+    },
+    time: {
+      get() {
+        return this.get_time;
+      },
+      set(value) {
+        this.set_time(value);
+      }
+    }
+  },
+  methods: {
+    ...mapMutations("post", {
+      set_time: "set_time",
+      set_date: "set_date"
+    })
   }
 };
 </script>
@@ -27,6 +56,7 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  flex-wrap: wrap;
   margin: var(--space-sm);
 
   div:first-child {
